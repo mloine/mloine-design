d7458bbadb79ba18b0ace3d86e45804544c5c345
薛勇康 <xueyongkang@tongyangstock.com>
2019-11-11 18:09:59 +0800

    1.redis基本介绍 2.lua语言基本介绍安装 ip访问限流例子

---
+++ src/main/java/com/mloine/redis/package-info.java
@@ -0,0 +1,26 @@
+package com.mloine.redis;
+
+/**
+ *  redis  缓存工具
+ *  Redis是一个开源的使用ANSI C语言编写、支持网络、可基于内存亦可持久化的日志型、Key-Value数据库，并提供多种语言的API
+ *                      1，Redis安装在磁盘；
+ *                      2，Redis数据存储在内存。
+ *
+ *  1.redis中的数据类型   string hash list set zset
+ *      a.string 字符串
+ *      b.hash 哈希
+ *      c.list 列表
+ *      d.set   无序集合
+ *      e.zset  有序集合
+ *
+ *  2.redis的特点
+ *      a.速度快
+ *      b.键值对的数据结构
+ *      c.丰富的功能
+ *      d.简单稳定
+ *      e.持久化
+ *      f.主从复制
+ *      g.高可用和分布式转移
+ *      h.客户端语言多
+ *
+ **/
